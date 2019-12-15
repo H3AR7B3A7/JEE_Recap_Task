eac013666769716c6683211a33e67e18c7fcd712
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2019-12-15 18:42:43 +0100

    Import cleanup

--- src/main/java/be/steven/d/dog/recapjee/ctrl/ProductCtrl.java
+++ src/main/java/be/steven/d/dog/recapjee/ctrl/ProductCtrl.java
@@ -6,13 +6,9 @@ import be.steven.d.dog.recapjee.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
 import java.util.Optional;
 
 /**
